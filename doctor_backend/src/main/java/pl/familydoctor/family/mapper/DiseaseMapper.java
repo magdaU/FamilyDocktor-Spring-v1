e86d425940a9f86b8fbfeebb6baf0a4f8dade0f7
Magda <s9199@pjwstk.edu.pl>
2018-02-17 16:21:12 +0100

    Mapper to Dto

--- doctor_backend/src/main/java/pl/familydoctor/family/mapper/DiseaseMapper.java
+++ doctor_backend/src/main/java/pl/familydoctor/family/mapper/DiseaseMapper.java
@@ -5,6 +5,7 @@ import pl.familydoctor.family.domain.Disease;
 import pl.familydoctor.family.resource.dto.DiseaseDto;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,7 +24,14 @@ public class DiseaseMapper implements Mapper<DiseaseDto, Disease> {
 
     @Override
     public DiseaseDto convertToDto(Disease entity) {
-        return null;
+        DiseaseDto diseaseDto = new DiseaseDto();
+        diseaseDto.setId(entity.getId());
+        diseaseDto.setDiagnosis(entity.getDiagnosis());
+        diseaseDto.setIsDoctorDiagnosis(entity.getDoctorDiagnosis());
+        diseaseDto.setSymptom(entity.getSymptom());
+        diseaseDto.setDiseaseStartDate(entity.getDiseaseStartDate());
+        diseaseDto.setDiseaseEndDate(entity.getDiseaseEndDate());
+        return diseaseDto;
     }
 
     @Override
@@ -33,6 +41,11 @@ public class DiseaseMapper implements Mapper<DiseaseDto, Disease> {
 
     @Override
     public List<DiseaseDto> convertToDtos(List<Disease> entities) {
-        throw new NotImplementedException();
+        List<DiseaseDto> diseaseDtoList = new ArrayList<>();
+        entities.forEach(disease -> {
+            DiseaseDto diseaseDto = convertToDto(disease);
+            diseaseDtoList.add(diseaseDto);
+        });
+        return diseaseDtoList;
     }
 }
