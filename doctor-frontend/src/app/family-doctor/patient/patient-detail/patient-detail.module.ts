dce33b02e8f6cfd4c2211d6d4d7f025e045815ae
Magda <s9199@pjwstk.edu.pl>
2018-02-24 16:59:13 +0100

    Create details with sample tab view and change to material add patient form

--- doctor-frontend/src/app/family-doctor/patient/patient-detail/patient-detail.module.ts
+++ doctor-frontend/src/app/family-doctor/patient/patient-detail/patient-detail.module.ts
@@ -5,13 +5,15 @@ import { PatientDetailRoutingModule } from './patient-detail-routing.module';
 import { PatientDetailComponent } from './patient-detail.component';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     PatientDetailRoutingModule,
     MatCardModule,
-    MatTabsModule
+    MatTabsModule,
+    MatButtonModule
   ],
   declarations: [PatientDetailComponent]
 })
