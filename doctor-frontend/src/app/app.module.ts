d17b91d562b00f135daa4cecc178b07bbbd5c6f4
Magda <s9199@pjwstk.edu.pl>
2018-02-24 13:44:34 +0100

    Create material

--- doctor-frontend/src/app/app.module.ts
+++ doctor-frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {
   MatToolbarModule,
   MatCardModule,
 } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,7 @@ import {
     MatButtonModule,
     MatMenuModule,
     MatToolbarModule,
-    MatCardModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
